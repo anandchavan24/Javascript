32af82cb5f248c8008f66eadca7a0180de3d507f
anandchavan24 <anand.chavan@joshsoftware.com>
2024-03-29 11:10:16 +0530

    Changes as requested by reviewer done

--- 0.6 JS Fundamentals -Arrays/Assignment1.js
+++ 0.6 JS Fundamentals -Arrays/Assignment1.js
@@ -2,58 +2,57 @@
 
 // 1. Functions for Array Operations:
 
-// ```javascript
-// const data = [{
-//     "id": 1,
-//     "first_name": "Nicki",
-//     "email": "ncrozier0@squarespace.com",
-//     "date_of_birth": "2009/05/09"
-// }, {
-//     "id": 2,
-//     "first_name": "Raychel",
-//     "email": "rmcgrady1@cpanel.net",
-//     "date_of_birth": "1996/11/05"
-// }, {
-//     "id": 3,
-//     "first_name": "Demetris",
-//     "email": "dkilshall2@elpais.com",
-//     "date_of_birth": "2018/12/31"
-// }, {
-//     "id": 4,
-//     "first_name": "Amata",
-//     "email": "abraiden3@canalblog.com",
-//     "date_of_birth": "2012/05/23"
-// }, {
-//     "id": 5,
-//     "first_name": "Venita",
-//     "email": "vheap4@clickbank.net",
-//     "date_of_birth": "2020/10/04"
-// }, {
-//     "id": 6,
-//     "first_name": "Fairfax",
-//     "email": "fcrichton5@merriam-webster.com",
-//     "date_of_birth": "2009/12/23"
-// }, {
-//     "id": 7,
-//     "first_name": "Kathleen",
-//     "email": "kvasyukhnov6@devhub.com",
-//     "date_of_birth": "2010/12/20"
-// }, {
-//     "id": 8,
-//     "first_name": "Sam",
-//     "email": "scorck7@sitemeter.com",
-//     "date_of_birth": "2020/08/30"
-// }, {
-//     "id": 9,
-//     "first_name": "Virgilio",
-//     "email": "vferandez8@e-recht24.de",
-//     "date_of_birth": "2000/09/07"
-// }, {
-//     "id": 10,
-//     "first_name": "Townie",
-//     "email": "tpetyt9@upenn.edu",
-//     "date_of_birth": "2018/09/01"
-// }];
+const data = [{
+    "id": 1,
+    "first_name": "Nicki",
+    "email": "ncrozier0@squarespace.com",
+    "date_of_birth": "2009/05/09"
+}, {
+    "id": 2,
+    "first_name": "Raychel",
+    "email": "rmcgrady1@cpanel.net",
+    "date_of_birth": "1996/11/05"
+}, {
+    "id": 3,
+    "first_name": "Demetris",
+    "email": "dkilshall2@elpais.com",
+    "date_of_birth": "2018/12/31"
+}, {
+    "id": 4,
+    "first_name": "Amata",
+    "email": "abraiden3@canalblog.com",
+    "date_of_birth": "2012/05/23"
+}, {
+    "id": 5,
+    "first_name": "Venita",
+    "email": "vheap4@clickbank.net",
+    "date_of_birth": "2020/10/04"
+}, {
+    "id": 6,
+    "first_name": "Fairfax",
+    "email": "fcrichton5@merriam-webster.com",
+    "date_of_birth": "2009/12/23"
+}, {
+    "id": 7,
+    "first_name": "Kathleen",
+    "email": "kvasyukhnov6@devhub.com",
+    "date_of_birth": "2010/12/20"
+}, {
+    "id": 8,
+    "first_name": "Sam",
+    "email": "scorck7@sitemeter.com",
+    "date_of_birth": "2020/08/30"
+}, {
+    "id": 9,
+    "first_name": "Virgilio",
+    "email": "vferandez8@e-recht24.de",
+    "date_of_birth": "2000/09/07"
+}, {
+    "id": 10,
+    "first_name": "Townie",
+    "email": "tpetyt9@upenn.edu",
+    "date_of_birth": "2018/09/01"
+}];
 
 // // Task 1a: filterByName function
 const filterByName = (str) => data.filter(obj => obj.first_name.includes(str));
@@ -68,10 +67,46 @@ const sortByDateOfBirthDesc = data.sort((a, b) => new Date(b.date_of_birth) - ne
 const getById = (id) => data.find(obj => obj.id === id);
 
 // Testing the functions
-console.log(filterByName("am")); // Filters by name containing "am"
-console.log(getEmails); // Returns an array of all email fields
+// console.log(filterByName("am")); // Filters by name containing "am"
+
+// [
+//   {
+//     id: 8,
+//     first_name: 'Sam',
+//     email: 'scorck7@sitemeter.com',
+//     date_of_birth: '2020/08/30'
+//   }
+// ]
+
+
+// console.log(getEmails); // Returns an array of all email fields
+// [
+//     'ncrozier0@squarespace.com',
+//     'rmcgrady1@cpanel.net',
+//     'dkilshall2@elpais.com',
+//     'abraiden3@canalblog.com',
+//     'vheap4@clickbank.net',
+//     'fcrichton5@merriam-webster.com',
+//     'kvasyukhnov6@devhub.com',
+//     'scorck7@sitemeter.com',
+//     'vferandez8@e-recht24.de',
+//     'tpetyt9@upenn.edu'
+// ]
+
+
 console.log(sortByDateOfBirthDesc); // Returns the sorted array
+
+// The array is very big to show here again
+
+
 console.log(getById(3)); // Returns object with id 3
+
+/*{
+  id: 3,
+  first_name: 'Demetris',
+  email: 'dkilshall2@elpais.com',
+  date_of_birth: '2018/12/31'
+}*/
 
 // 2. Mutating vs. Non-Mutating Methods in JavaScript:
 
